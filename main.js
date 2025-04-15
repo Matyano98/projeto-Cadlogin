7a19e1e99600011f1ce39ffa705d3ab8e4c9687f
Mateus yano <mattochioyano98@gmail.com>
2025-04-15 19:49:14 -0300

    Adicionando arquivo css para pagina de login e mensagens de aviso e implementando pagina para cadastro

---
+++ main.js
@@ -0,0 +1,13 @@
+// Implementa acesso para a tela de login
+function acessar() {
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+
+    // Implementa estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha) {
+        alert("Preencha todos os campos!");
+        return;
+    } else{
+        window.location.href = "cadastro.html";
+    }
+}
