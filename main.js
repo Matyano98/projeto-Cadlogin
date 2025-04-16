fe826449942d7ff2c75865936e8ea2de2b5fac9d
Mateus yano <mattochioyano98@gmail.com>
2025-04-15 21:33:01 -0300

    adiciona tabela e salvando nomes com a opção de editar ou excluir

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ function acessar() {
         window.location.href = "cadastro.html";
     }
 }
+
+// cria variável do tipo array
+var dadosLista = [];
+
+function salvarUser() {
+    let nomeUser = document.getElementById("nomeUser").value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+        criaLista();
+        document.getElementById("nomeUser").value = "";
+    }else{
+        alert("favor informar um nome para cadastro");
+    }
+    
+}   
+
+// Função para preencher a lista de cadastro
+    function criaLista() {
+        let tabela = "<tr><th>Nome</th<th>Ações</th></tr>";
+        for (let i = 0;i <=(dadosLista.length - 1); i++){
+            tabela += "<tr><td>" + dadosLista[i] + "</td<td><button class='btn btn-warning'>Editar</button><button class='btn btn-danger'>Excluir</button></td></tr>";
+            document.getElementById("tabela").innerHTML = tabela;
+        }
+    }
+
+    // Função para excluir nome da lista
+
+    function excluir(i){
+        dadosLista.splice((i-1), 1);
+        criaLista();
+    }
